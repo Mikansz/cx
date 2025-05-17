6632f9186f359aa3d95345a8bd1ea60392a27c48
yusukesan85 <yusukesan85@gmail.com>
2025-05-17 00:26:41 +0000

    mi

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Presensi;
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('presensi', Presensi::class)->name('presensi');
+    Route::get('attendance/export', function () {
+        return Excel::download(new AttendanceExport, 'attendances.xlsx');
+    })->name('attendance-export');
+    
+    
+});
+
+Route::get('/login', function() {
+    return redirect('admin/login');
+})->name('login');
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
