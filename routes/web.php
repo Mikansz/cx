4805d016aabc4b4a0be6e502c9bc8b085a019947
yusukesan85 <yusukesan85@gmail.com>
2025-05-18 21:13:45 +0000

    ok

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Presensi;
+use App\Exports\AttendanceExport;
+
 Route::group(['middleware' => 'auth'], function() {
     Route::get('presensi', Presensi::class)->name('presensi');
     Route::get('attendance/export', function () {
